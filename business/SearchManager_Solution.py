b3c7127bd4d78745d7dbc4258a4e434c111200b6
Phillip Gachnang <pgachnang@gmail.com>
2024-04-29 06:34:21 +0200

    Lecture UE3_2 solution

--- business/SearchManager_Solution.py
+++ business/SearchManager_Solution.py
@@ -14,49 +14,73 @@ class SearchManager(object):
 
     def __load_db(self):
         # Ensure the environment Variable is set
-        db_file = os.environ.get('DB_FILE')  # Lesen einer Umgebungsvariable
-
-        if not db_file:
+        if not os.environ.get("DB_FILE"):
             raise ValueError("You have to define the environment variable 'DB_FILE'")
-        self.__db_filepath = Path(db_file)
+        self.__db_filepath = Path(os.environ.get("DB_FILE"))
 
-        # Ensure the db file exists, if not create a new db file with or without example data
+        # Ensure the db file exists, if not initialize a new db with or without example data
         # You have to delete the db file, if you need a new fresh db.
         if not self.__db_filepath.is_file():
-            init_db(db_file, generate_example_data=True)
+            init_db(str(self.__db_filepath), generate_example_data=True)
 
-        # create the engine and the session.
-        # the engine is private, no need for subclasses to be able to access it.
-        self._engine = create_engine(f'sqlite:///{db_file}')
-        # create the session as db connection
-        # subclasses need access therefore, protected attribute so every inheriting manager has access to the connection
+        self._engine = create_engine(f'sqlite:///{self.__db_filepath}')
         self._session = scoped_session(sessionmaker(bind=self._engine))
 
-    def get_all_hotels(self):
+    def get_hotels(self, name: str = "", city: str = ""):
         query = select(Hotel)
+        if name != "":
+            query = query.where(Hotel.name.like(f'%{name}%'))
+        if city != "":
+            query = query.join(Address).where(Address.city.like(f"%{city}%"))
+
+        print(query)
         return self._session.execute(query).scalars().all()
 
-    def get_hotels_by_city(self, city: str):
-        query = select(Hotel).join(Address).where(Address.city == city)
+    def get_hotels_by_name(self, name: str = ""):
+        query = select(Hotel)
+        if name != "":
+            query = query.where(Hotel.name.like(f'%{name}'))
+        print(query)
         return self._session.execute(query).scalars().all()
+
+    def get_hotels_by_city(self, city: str = ""):
+        query = select(Hotel)
+        if city != "":
+            query = query.join(Address).where(Address.city.like(f"%{city}%"))
+        print(query)
+        return self._session.execute(query).scalars().all()
+
+
+def show(hotels):
+    for hotel in hotels:
+        print(hotel)
 
 
 if __name__ == '__main__':
     # This is only for testing without Application
 
-    # You should set the variable in the run configuration
+    # You should set the environment variable in the run configuration.
+    # However, we can set it here in python as well.
     # Because we are executing this file in the folder ./business/
     # we need to relatively navigate first one folder up and therefore,
     # use ../data in the path instead of ./data
     # if the environment variable is not set, set it to a default
-    if not os.environ.get('DB_FILE'):
-        os.environ['DB_FILE'] = '../data/test.db'
+    if not os.environ.get("DB_FILE"):
+        os.environ["DB_FILE"] = input("Enter relative Path to db file: ")
+        while not Path(os.environ.get("DB_FILE")).parent.is_dir():
+            os.environ["DB_FILE"] = input("Enter relative Path to db file: ")
     search_manager = SearchManager()
     all_hotels = search_manager.get_all_hotels()
-    for hotel in all_hotels:
-        print(hotel)
+    show(all_hotels)
+    input("Press Enter to continue...")
 
-    city_user = input('City: ')
-    hotels = search_manager.get_hotels_by_city(city_user)
-    for hotel in hotels:
-        print(hotel)
+    city_in = input('City: ')
+    hotels_by_city = search_manager.get_hotels_by_city(city_in)
+    show(hotels_by_city)
+    input("Press Enter to continue...")
+
+    name_in = input('Name: ')
+    city_in = input('City: ')
+    found_hotels = search_manager.get_hotels(name_in, city_in)
+    show(found_hotels)
+    input("Press Enter to continue...")
---
+++ main.py
@@ -0,0 +1,3 @@
+from business.SearchManager_Solution import SearchManager
+
+if __name__ == '__main__':
